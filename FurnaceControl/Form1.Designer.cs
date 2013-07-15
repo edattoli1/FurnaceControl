c3f021251729386b078e1968a0071d4cb45e8b0f
Eric D <minsey@gmail.com>
2013-07-15 06:44:40 -0400

    1

---
+++ FurnaceControl/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FurnaceControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.onButton = new System.Windows.Forms.Button();
+            this.offButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // onButton
+            // 
+            this.onButton.Location = new System.Drawing.Point(42, 103);
+            this.onButton.Name = "onButton";
+            this.onButton.Size = new System.Drawing.Size(59, 23);
+            this.onButton.TabIndex = 0;
+            this.onButton.Text = "ON";
+            this.onButton.UseVisualStyleBackColor = true;
+            this.onButton.Click += new System.EventHandler(this.onButton_Click);
+            // 
+            // offButton
+            // 
+            this.offButton.Location = new System.Drawing.Point(42, 132);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(75, 23);
+            this.offButton.TabIndex = 1;
+            this.offButton.Text = "OFF";
+            this.offButton.UseVisualStyleBackColor = true;
+            this.offButton.Click += new System.EventHandler(this.offButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.offButton);
+            this.Controls.Add(this.onButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button onButton;
+        private System.Windows.Forms.Button offButton;
+    }
+}
+
---
+++ FurnaceControl/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Globalization;
+
+
+namespace FurnaceControl
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort port;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void onButton_Click(object sender, EventArgs e)
+        {
+            port.Open();
+            // \x0201010WWRD0121,01,0001\x03
+            port.Write(Int32.Parse("02", NumberStyles.HexNumber) + "01010WWRD0121,01,0001" + Int32.Parse("02", NumberStyles.HexNumber) +Int32.Parse("0D", NumberStyles.HexNumber) ) ;
+            port.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            port = new SerialPort("COM1", 9600, Parity.Even, 8, StopBits.One);
+        }
+
+        private void offButton_Click(object sender, EventArgs e)
+        {
+            port.Open();
+            // \x0201010WWRD0121,01,0000\x03
+
+            port.Write(Int32.Parse("02", NumberStyles.HexNumber) + "01010WWRD0121,01,0000" + Int32.Parse("02", NumberStyles.HexNumber) +Int32.Parse("0D", NumberStyles.HexNumber) ) ;
+            port.Close();
+        }
+    }
+}
