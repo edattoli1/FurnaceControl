3acca3c546cb6988e4e03f9b472b0b8e115d7de0
Eric D <minsey@gmail.com>
2013-07-15 07:00:25 -0400

    4

--- FurnaceControl/Form1.cs
+++ FurnaceControl/Form1.cs
@@ -27,21 +27,27 @@ namespace FurnaceControl
         {
             port.Open();
             // \x0201010WWRD0121,01,0001\x03
-            port.Write( (char) 2 + "01010WWRD0121,01,0001" + (char) 2 + '\r' ) ;
-            port.Close();
+            port.Write( (char) 2 + "01010WWRD0121,01,0001" + (char) 3 + '\r' ) ;
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             port = new SerialPort("COM1", 9600, Parity.Even, 8, StopBits.One);
+            port.Open();
         }
 
         private void offButton_Click(object sender, EventArgs e)
         {
-            port.Open();
+            
             // \x0201010WWRD0121,01,0000\x03   Int32.Parse("0D", NumberStyles.HexNumber)
 
-            port.Write( (char) 2 + "01010WWRD0121,01,0000" + (char) 2 + '\r' ) ;
+            port.Write( (char) 2 + "01010WWRD0121,01,0000" + (char) 3 + '\r' ) ;
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
             port.Close();
         }
     }
